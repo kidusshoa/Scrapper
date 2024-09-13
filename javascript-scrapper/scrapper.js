1ee1490f67c9e391e5dda6f6841d54a90b62684d
kidusshoa <captainkevin2008@gmail.com>
2024-09-13 11:22:34 +0300

    pages scrape

--- javascript-scrapper/scrapper.js
+++ javascript-scrapper/scrapper.js
@@ -5,31 +5,45 @@ const scrape = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const url = "https://books.toscrape.com";
+  const allBooks = [];
+  let currentPage = 1;
+  const maxPages = 10;
 
-  await page.goto(url);
+  //const url = "https://books.toscrape.com";
 
-  const books = await page.evaluate(() => {
-    const bookElements = document.querySelectorAll(".product_pod");
-    return Array.from(bookElements).map((book) => {
-      const title = book.querySelector("h3 a").getAttribute("title");
-      const price = book.querySelector(".price_color").textContent;
-      const stock = book.querySelector(".instock.availablity")
-        ? "In Stock"
-        : "Out Of Stock";
-      const rating = book.querySelector(".star-rating").className.split(" ")[1];
-      const link = book.querySelector("h3 a").getAttribute("href");
-      return {
-        title,
-        price,
-        stock,
-        rating,
-        link,
-      };
+  while (currentPage <= maxPages) {
+    const url = `https://books.toscrape.com/catalogue/page-${currentPage}.html`;
+
+    await page.goto(url);
+
+    const books = await page.evaluate(() => {
+      const bookElements = document.querySelectorAll(".product_pod");
+      return Array.from(bookElements).map((book) => {
+        const title = book.querySelector("h3 a").getAttribute("title");
+        const price = book.querySelector(".price_color").textContent;
+        const stock = book.querySelector(".instock.availablity")
+          ? "In Stock"
+          : "Out Of Stock";
+        const rating = book
+          .querySelector(".star-rating")
+          .className.split(" ")[1];
+        const link = book.querySelector("h3 a").getAttribute("href");
+        return {
+          title,
+          price,
+          stock,
+          rating,
+          link,
+        };
+      });
     });
-  });
 
-  fs.writeFileSync("books.json", JSON.stringify(books, null, 2));
+    allBooks.push(...books);
+    console.log(`Books on page ${currentPage}`, books);
+    currentPage++;
+  }
+
+  fs.writeFileSync("books.json", JSON.stringify(allBooks, null, 2));
 
   console.log("Data is Saved books.json");
 
