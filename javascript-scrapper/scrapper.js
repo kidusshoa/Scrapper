2c87f6f9a30b94b7b5656fc817fa3636e419094e
kidusshoa <captainkevin2008@gmail.com>
2024-09-13 10:58:18 +0300

    scrape and save

--- javascript-scrapper/scrapper.js
+++ javascript-scrapper/scrapper.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
 
 const scrape = async () => {
   const browser = await puppeteer.launch();
@@ -8,9 +9,29 @@ const scrape = async () => {
 
   await page.goto(url);
 
-  const title = await page.title();
+  const books = await page.evaluate(() => {
+    const bookElements = document.querySelectorAll(".product_pod");
+    return Array.from(bookElements).map((book) => {
+      const title = book.querySelector("h3 a").getAttribute("title");
+      const price = book.querySelector(".price_color").textContent;
+      const stock = book.querySelector(".instock.availablity")
+        ? "In Stock"
+        : "Out Of Stock";
+      const rating = book.querySelector(".star-rating").className.split(" ")[1];
+      const link = book.querySelector("h3 a").getAttribute("href");
+      return {
+        title,
+        price,
+        stock,
+        rating,
+        link,
+      };
+    });
+  });
 
-  console.log(`Page Title: ${title}`);
+  fs.writeFileSync("books.json", JSON.stringify(books, null, 2));
+
+  console.log("Data is Saved books.json");
 
   await browser.close();
 };
